31334be443101e92d3095f2760df62e81793b22f
hismile06jf <hismile06jf@163.com>
2013-11-19 04:18:44 -0800

    1、animation type

---
+++ Assets/Script/AssetMgr/AnimType.cs
@@ -0,0 +1,58 @@
+
+
+public enum EAnimType
+{
+	//BaseType
+	Idle,
+	Idle_a,
+	
+	Rest_Down,
+	Rest_Idle,
+	Rest_Idle_a,
+	Rest_Up,
+	
+	Walk_Fore,
+	Walk_Back,
+	Walk_TurnLeft,
+	Walk_TurnRight,
+	Walk_Die,
+	
+	Run_Fore,
+	Run_Back,
+	Run_RurnLeft,
+	Run_RurnRight,
+	Run_Die,
+	
+	//Other
+	Swim_Fore,
+	Swim_Back,
+	Swim_TurnLeft,
+	Swim_TurnRight,
+	Swim_Die,
+	
+	Jump_Begin,
+	Jump_Hold,
+	Jump_End,
+	Jump_EndRun,
+	
+	Escape_a,
+	Escape_b,
+	Escape_c,
+	
+	Weapon_LHOn,
+	Weapon_LHOff,
+	
+	Weapon_RHOn,
+	Weapon_RHOff,
+	
+	Weapon_DHOn,
+	Weapon_DHOff,
+	
+	Defense,
+	
+	Hit_a,
+	Hit_horse,
+	
+	//Other
+}
+
